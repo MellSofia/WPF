d2fcca50b55aad2e7929203a9f31bde32af1f623
MellSofia <Sofaahmutdinova@gmail.com>
2024-10-22 19:20:53 +0400

    22.10.2024

---
+++ WpfAppProtoBD/WpfAppProtoBD/EditUserWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppProtoBD
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private MainWindow form1;
+        private List<User> users_ = new List<User>();
+        public Window1(MainWindow _form1_)
+        {
+            InitializeComponent();
+            form1 = _form1_;
+            users_ = _form1_.users;
+
+            // Добавляем пользователей в comboBox, исключая администратора
+            foreach (var item in users_)
+            {
+                if (item.LastName != "admin") // Исключаем администратора
+                {
+                    comboBoxUsers.Items.Add(item.LastName);
+                }
+            }
+
+            foreach (string position in _form1_.positions)
+            {
+                comboBox_position.Items.Add(position);
+
+            }
+        }
+
+        private void ClearcomboBoxUsersFields()
+        {
+            textBox_name.Text = string.Empty;
+            textBox_password.Text = string.Empty;
+            comboBox_position.SelectedItem = null;
+            textBox_homer_num.Text = string.Empty;
+            textBox_mobile_numr.Text = string.Empty;
+            checkBox_homer_num.IsChecked = false;
+            checkBox_mobile_numr.IsChecked = false;
+            radioButton_man.IsChecked = false;
+            radioButton_woman.IsChecked = false;
+
+            // Очищаем метки ошибок
+            label_name_check.Content = "";
+            label_surename_check.Content = "";
+            label_position_check.Content = "";
+            label_password_check.Content = "";
+            label_homer_num_check.Content = "";
+            label_mobile_numr_check.Content = "";
+            label_gender_check.Content = "";
+        }
+        private void Button_save_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsInputValid())
+            {
+                UpdateUserDetails();
+                MessageBox.Show("Изменения прошли успешно!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                comboBoxUsers.SelectedItem = null;
+                ClearcomboBoxUsersFields();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, заполните все обязательные поля.");
+            }
+        }
+
+        private bool IsInputValid()
+        {
+            return !string.IsNullOrWhiteSpace(textBox_name.Text) &&
+                   !string.IsNullOrWhiteSpace(comboBox_position.Text) &&
+                   !string.IsNullOrWhiteSpace(textBox_password.Text) &&
+                   (checkBox_homer_num.IsChecked == false || !string.IsNullOrWhiteSpace(textBox_homer_num.Text)) &&
+                   (checkBox_mobile_numr.IsChecked == false || !string.IsNullOrWhiteSpace(textBox_mobile_numr.Text)) &&
+                   ((radioButton_man.IsChecked == true || radioButton_woman.IsChecked == true));
+        }
+
+        private void UpdateUserDetails()
+        {
+            var selectedUser = users_.Find(u => u.LastName == comboBoxUsers.Text);
+            if (selectedUser != null)
+            {
+                selectedUser.FirstName = textBox_name.Text;
+                selectedUser.Gender = radioButton_man.IsChecked == true ? "Мужской" : "Женский";
+                selectedUser.Position = comboBox_position.SelectedItem.ToString();
+                selectedUser.HomePhone = checkBox_homer_num.IsChecked == true ? textBox_homer_num.Text : null;
+                selectedUser.MobilePhone = checkBox_mobile_numr.IsChecked == true ? textBox_mobile_numr.Text : null;
+                selectedUser.Password = textBox_password.Text;
+            }
+        }
+
+        private void comboBoxUsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ClearcomboBoxUsersFields(); // Очищаем поля перед отображением новой информации
+
+            string lastName = (string)((ComboBox)sender).SelectedItem; // Получаем выбранное значение
+
+            User user = users_.Find(u => u.LastName == lastName);
+            if (user != null) // Проверяем, найден ли пользователь
+            {
+                textBox_name.Text = user.FirstName;
+                comboBox_position.Text = user.Position;
+                checkBox_homer_num.IsChecked = !string.IsNullOrEmpty(user.HomePhone);
+                checkBox_mobile_numr.IsChecked = !string.IsNullOrEmpty(user.MobilePhone);
+                textBox_homer_num.Text = user.HomePhone;
+                textBox_mobile_numr.Text = user.MobilePhone;
+                radioButton_man.IsChecked = user.Gender == "Мужской";
+                radioButton_woman.IsChecked = user.Gender == "Женский";
+                textBox_password.Text = user.Password;
+
+                textBox_mobile_numr.IsEnabled = checkBox_mobile_numr.IsChecked == true;
+                textBox_homer_num.IsEnabled = checkBox_homer_num.IsChecked == true;
+
+                form1.ValidateInput(comboBoxUsers, label_surename_check);
+            }
+        }
+
+
+        private void TextBox_name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            form1.ValidateTextBox(textBox_name);
+            form1.ValidateInput(textBox_name, label_name_check);
+        }
+
+        private void CheckBox_homer_num_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox_homer_num.IsEnabled = true; // Включаем текстовое поле
+        }
+
+        private void CheckBox_homer_num_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBox_homer_num.IsEnabled = false; // Отключаем текстовое поле
+            textBox_homer_num.Text = string.Empty; // Очищаем текстовое поле при отключении
+        }
+
+        private void CheckBox_mobile_numr_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox_mobile_numr.IsEnabled = true; // Включаем текстовое поле
+        }
+
+        private void CheckBox_mobile_numr_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBox_mobile_numr.IsEnabled = false; // Отключаем текстовое поле
+            textBox_mobile_numr.Text = string.Empty; // Очищаем текстовое поле при отключении
+        }
+
+        private void TextBox_mobile_numr_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            form1.ValidatePhoneNumber(textBox_mobile_numr);
+            form1.ValidateInput(textBox_mobile_numr, checkBox_mobile_numr, label_mobile_numr_check);
+        }
+
+        private void TextBox_homer_num_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            form1.ValidatePhoneNumber(textBox_homer_num);
+            form1.ValidateInput(textBox_homer_num, checkBox_homer_num, label_homer_num_check);
+        }
+
+        private void TextBox_password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            form1.ValidateInput(textBox_password, label_password_check);
+        }
+
+
+        private void button_back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void comboBox_position_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            form1.ValidateInput(comboBox_position, label_position_check);
+        }
+    }
+}
---
+++ WpfAppProtoBD/WpfAppProtoBD/MainWindow.xaml.cs
@@ -0,0 +1,388 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppProtoBD
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<User> users = new List<User>();
+        public String[] positions = new String[] { "Директор", "Управляющий", "Рабочий", "Маркетолог" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            users.Add(new User
+            {
+                LastName = "admin",
+                Password = "admin"
+            });
+            panel_regist.Visibility = Visibility.Hidden;
+            panel_login.Visibility = Visibility.Hidden;
+            panel_regist.Margin = new Thickness(50, 7, 36, 15);
+            panel_login.Margin = new Thickness(50, 7, 36, 15);
+            textBox_mobile_num.IsEnabled = false;
+            textBox_home_num.IsEnabled = false;
+            foreach (string position in positions)
+            {
+                comboBox_position.Items.Add(position);
+            }
+        }
+
+        private void button_regist_Click(object sender, RoutedEventArgs e)
+        {
+            panel_regist.Visibility = panel_regist.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            button_regist.Visibility = button_regist.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            button_login.Visibility = button_login.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+
+        }
+        private void button_back_Click(object sender, RoutedEventArgs e)
+        {
+            panel_regist.Visibility = panel_regist.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            button_login.Visibility = button_login.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            button_regist.Visibility = button_regist.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            ClearRegistrationFields();
+        }
+
+        private void button_login_Click(object sender, RoutedEventArgs e)
+        {
+            panel_login.Visibility = panel_login.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            button_regist.Visibility = button_regist.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            button_login.Visibility = button_login.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+
+            comboBoxSurname.Items.Clear();
+
+            foreach (var item in users)
+            {
+                comboBoxSurname.Items.Add(item.LastName);
+            }
+        }
+
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            panel_login.Visibility = panel_login.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            button_regist.Visibility = button_regist.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            button_login.Visibility = button_login.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            textBoxPassword.Text = string.Empty;
+        }
+
+        private void Button_sent_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox_name.Text) || string.IsNullOrWhiteSpace(textBox_surename.Text) ||
+                string.IsNullOrWhiteSpace(comboBox_position.Text) || string.IsNullOrWhiteSpace(textBox_password.Text) ||
+                (checkBox_home_num.IsChecked == true && string.IsNullOrWhiteSpace(textBox_home_num.Text)) ||
+                (checkBox_mobile_num.IsChecked == true && string.IsNullOrWhiteSpace(textBox_mobile_num.Text)) ||
+                (!radioButton_man.IsChecked == true && !radioButton_woman.IsChecked == true))
+            {
+                MessageBox.Show("Пожалуйста, заполните все обязательные поля.");
+                return;
+            }
+
+            if (users.Any(u => u.LastName == textBox_surename.Text && u.FirstName == textBox_name.Text))
+            {
+                MessageBox.Show("Такой пользователь уже существует.");
+                return;
+            }
+
+            User newUser = new User
+            {
+                FirstName = textBox_name.Text,
+                LastName = textBox_surename.Text,
+                Gender = radioButton_man.IsChecked == true ? "Мужской" : "Женский",
+                Position = comboBox_position.Text,
+                HomePhone = checkBox_home_num.IsChecked == true ? textBox_home_num.Text : null,
+                MobilePhone = checkBox_mobile_num.IsChecked == true ? textBox_mobile_num.Text : null,
+                Password = textBox_password.Text
+            };
+
+            users.Add(newUser);
+            MessageBox.Show("Регистрация прошла успешно!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            ClearRegistrationFields();
+
+        }
+        private void ClearRegistrationFields()
+        {
+            textBox_name.Clear();
+            textBox_surename.Clear();
+            comboBox_position.SelectedIndex = -1;
+            textBox_password.Clear();
+            checkBox_home_num.IsChecked = false;
+            textBox_home_num.Clear();
+            checkBox_mobile_num.IsChecked = false;
+            textBox_mobile_num.Clear();
+            radioButton_man.IsChecked = false;
+            radioButton_woman.IsChecked = false;
+        }
+        private void Button_enter_Click(object sender, RoutedEventArgs e)
+        {
+            string lastName = comboBoxSurname.Text;
+            string password = textBoxPassword.Text;
+
+            // Поиск пользователя по фамилии и паролю
+            User user = users.Find(u => u.LastName == lastName && u.Password == password);
+            if (user != null)
+            {
+                if (user.LastName == "admin" && user.Password == "admin")
+                {
+                    MessageBoxResult result = MessageBox.Show("Хотите ли вы войти как админ?", "Подтверждение", MessageBoxButton.YesNo);
+
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        Window1 editform = new Window1(this);
+                        editform.Owner = this;
+                        editform.ShowDialog();
+                    }
+                    else
+                    {
+                        MessageBox.Show(user.ShowInfo());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(user.ShowInfo());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка входа: неверная фамилия или пароль.");
+            }
+
+            ClearLoudFields();
+        }
+        private void ClearLoudFields()
+        {
+            comboBoxSurname.SelectedIndex = -1;
+            textBoxPassword.Text = string.Empty;
+        }
+
+        public void ValidatePhoneNumber(TextBox textBox)
+        {
+            string num = textBox.Text.Trim();
+
+            if (!string.IsNullOrEmpty(num) && !Int32.TryParse(num, out _))
+            {
+                MessageBox.Show("Ошибка ввода: номер должен состоять из цифр.");
+                textBox.Text = string.Empty; // Очищаем поле только после предупреждения
+            }
+        }
+
+        public void ValidateTextBox(TextBox textBox)
+        {
+            string input = textBox.Text.Trim();
+
+            // Проверяем, состоит ли ввод только из букв
+            if (!string.IsNullOrEmpty(input) && !input.All(char.IsLetter))
+            {
+                MessageBox.Show("Ошибка ввода: поле должно содержать только буквы.");
+                textBox.Text = string.Empty; // Очищаем поле только после предупреждения
+            }
+        }
+
+        public void ValidateInput(TextBox textBox, CheckBox checkBox, Label indicatorLabel)
+        {
+            string input = textBox.Text.Trim();
+            if (string.IsNullOrWhiteSpace(input) || !checkBox.IsChecked.GetValueOrDefault())
+            {
+                indicatorLabel.Content = "✗"; // Крестик для пустого поля
+                indicatorLabel.Foreground = Brushes.Red;
+            }
+            else
+            {
+                indicatorLabel.Content = "✓"; // Галочка для корректного ввода
+                indicatorLabel.Foreground = Brushes.Green;
+            }
+        }
+
+        public void ValidateInput(TextBox textBox, Label indicatorLabel)
+        {
+            string input = textBox.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                indicatorLabel.Content = "✗"; // Крестик для пустого поля
+                indicatorLabel.Foreground = Brushes.Red;
+            }
+            else
+            {
+                indicatorLabel.Content = "✓"; // Галочка для корректного ввода
+                indicatorLabel.Foreground = Brushes.Green;
+            }
+        }
+
+        public void ValidateInput(ComboBox comboBox, Label indicatorLabel)
+        {
+            if (comboBox.SelectedItem == null)
+            {
+                indicatorLabel.Content = "✗"; // Крестик для пустого выбора
+                indicatorLabel.Foreground = Brushes.Red;
+            }
+            else
+            {
+                indicatorLabel.Content = "✓"; // Галочка для корректного выбора
+                indicatorLabel.Foreground = Brushes.Green;
+            }
+        }
+        private void CheckBox_home_num_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox_home_num.IsEnabled = true; // Включаем текстовое поле
+        }
+
+        private void CheckBox_home_num_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBox_home_num.IsEnabled = false; // Отключаем текстовое поле
+            textBox_home_num.Text = string.Empty; // Очищаем текстовое поле при отключении
+        }
+
+        private void CheckBox_mobile_num_Checked(object sender, RoutedEventArgs e)
+        {
+            textBox_mobile_num.IsEnabled = true; // Включаем текстовое поле
+        }
+
+        private void CheckBox_mobile_num_Unchecked(object sender, RoutedEventArgs e)
+        {
+            textBox_mobile_num.IsEnabled = false; // Отключаем текстовое поле
+            textBox_mobile_num.Text = string.Empty; // Очищаем текстовое поле при отключении
+        }
+        private void TextBox_mobile_num_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidatePhoneNumber(textBox_mobile_num);
+            ValidateInput(textBox_mobile_num, checkBox_mobile_num, label_mobile_num_check);
+        }
+
+        private void TextBox_home_num_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidatePhoneNumber(textBox_home_num);
+            ValidateInput(textBox_home_num, checkBox_home_num, label_home_num_check);
+        }
+
+        private void TextBox_surename_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidateTextBox(textBox_surename);
+            ValidateInput(textBox_surename, label_surename_check);
+        }
+
+        private void TextBox_name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidateTextBox(textBox_name);
+            ValidateInput(textBox_name, label_name_check);
+        }
+
+
+
+        private void TextBox_password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidateInput(textBox_password, label_password_check);
+        }
+
+        private void comboBox_position_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ValidateInput(comboBox_position, label_position_check);
+        }
+    }
+    public class User : INotifyPropertyChanged
+    {
+        private string lastName;
+        private string firstName;
+        private string position;
+        private string homePhone;
+        private string mobilePhone;
+        private string gender;
+        private string password;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string LastName
+        {
+            get => lastName;
+            set => SetProperty(ref lastName, value);
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            set => SetProperty(ref firstName, value);
+        }
+
+        public string Position
+        {
+            get => position;
+            set => SetProperty(ref position, value);
+        }
+
+        public string HomePhone
+        {
+            get => homePhone;
+            set => SetProperty(ref homePhone, value);
+        }
+
+        public string MobilePhone
+        {
+            get => mobilePhone;
+            set => SetProperty(ref mobilePhone, value);
+        }
+
+        public string Gender
+        {
+            get => gender;
+            set => SetProperty(ref gender, value);
+        }
+
+        public string Password
+        {
+            get => password;
+            set => SetProperty(ref password, value);
+        }
+
+        public string ShowInfo()
+        {
+            string text = "Вход выполнен успешно!" +
+                          "\n Фамилия: " + this.LastName +
+                          "\n Имя: " + this.FirstName +
+                          "\n Должность: " + this.Position;
+            if (!string.IsNullOrEmpty(this.HomePhone))
+            {
+                text += "\n Домашний номер: " + this.HomePhone;
+            }
+            if (!string.IsNullOrEmpty(this.MobilePhone))
+            {
+                text += "\n Мобильный номер: " + this.MobilePhone;
+            }
+            text += "\n Пол: " + this.Gender;
+            return text;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
